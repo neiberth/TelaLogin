3d14b71ca6351e0e63911a2895c879db7aa0c91a
neiberth <neiberth@gmail.com>
2020-01-08 15:32:23 -0300

    Navegando entre Activity

--- app/src/main/java/com/neiberth/telalogin/LoginActivity.java
+++ app/src/main/java/com/neiberth/telalogin/LoginActivity.java
@@ -2,7 +2,10 @@ package com.neiberth.telalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logar(View view){
+
+        Intent logarPrincipal = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(logarPrincipal);
     }
 }
